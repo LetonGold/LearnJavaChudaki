354352400bd10631621d67331c130faa42b80506
Alexey Gavuk <warsager109@gmail.com>
2015-11-23 15:30:53 +0200

    assiment in java 3.2
    this is code for assiment 3.2. will do for now but will be changed in
    future

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,26 @@
+import java.lang.Math;
+
 public class Calculate{
     public static void main(String[] args) {
-        System.out.println("Calculate...");
+        int first = Integer.valueOf(args[0]);
+        String SymAction = args[1];
+        int second = Integer.valueOf(args[2]);
+        double result = 0;
+
+        switch(SymAction){
+            case "+": result = first + second;
+                break;
+            case "-": result = first - second;
+                break;
+            case "x": result = first * second;
+                break;
+            case "/": result = first / second;
+                break;
+            case "pow": result = Math.pow(first, second);
+		break;
+            default : System.out.println("Invalid expression.");
+                break;
+        }
+        System.out.println(result);
     }
 }
