b22afd7e8ba6b14279400079c8d1ee57c5c11a36
WARsageR <warsager109@gmail.com>
2015-11-25 14:04:38 +0200

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,16 +1,16 @@
-import java.lang.Math;
+import java.lang.Math; //Импорт библиотеки с математическими методами
 
-public class Calculator{
-	private int result;
+public class Calculator{ //Создание класса Калькулятор, совпадающее с названием файла в котором написан данный класс(иначе не сработает)
+	private int result; //приватная(доступна только в этом классе) integer(целочисленная) переменная под названием result, в которую будет записываться результат математической операции.
 	
-	public void add(int ... parameters){
-		for(Integer param: parameters){
-			this.result += param;		
+	public void add(int ... parameters){ //метод названный add который выполняет функцию сложения. Параметры попадающие в метод сохраняются в интовый массив с названием parameters
+		for(Integer param: parameters){ //цикл for в котором выполняется действие в блоке кода, столько раз, сколько находится в массиве parameters значений
+			this.result += param; // в this.result "this" выполняет функцию указателя на переменную result в "этом" конкретном объекте. += param добавляет к значению result текущее значение переменной param		
 		}	
 	}
 		
-	public void sub(int ... parameters){
-		this.result = parameters[0] - parameters[1];
+	public void sub(int ... parameters){ //метод называнный мной sub который выполняет функцию вычитания. Параметры записываются в массив parameters
+		this.result = parameters[0] - parameters[1]; // в переменную result этого объекта будет записан результат вычитания числа в первом индексе массива parameters из числа в нулевом индексе массива parameters(в джава индексы начинаются с "0") 
 	}
 	
 	public void mul(int ... parameters){
